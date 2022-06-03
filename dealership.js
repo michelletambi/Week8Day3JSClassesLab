d54b63fe9daa9050532906e9ea26c0104ed74750
Michelle Tambi <michelletambi22@gmail.com>
2022-06-03 12:42:29 +0100

    added dealership test for count() and addCar() functions

--- dealership.js
+++ dealership.js
@@ -1,22 +1,15 @@
 const Car = require('./car')
+
 const Dealership = function(name, maxCars, currentCars){
     this.name = name;
     this.maxCars = maxCars;
     this.currentCars = currentCars;
 }
+
 // this function counts the number of cars in a dealership
 Dealership.prototype.count = function(){
     return this.currentCars.length;
 }
-// make some cars
-const car1 = new Car("tesla", 5, "3 cylinder");
-const car2 = new Car("mercedes", 6, "4 cylinder");
-
-const dealer = new Dealership(
-    "pushers",
-    5,
-    [car1]
-)
 
 // this function adds a car to the dealership.
 Dealership.prototype.addCar = function(car){
@@ -29,13 +22,11 @@ Dealership.prototype.getManufacturers = function() {
     console.log(manufacturers);
 }
 
-
 // Find all cars in dealership of same manufacturer
 Dealership.prototype.findByManufacturer = function(manufacturer){
     const sisters = this.currentCars.filter(e => e.manufacturer === manufacturer);
     console.log(sisters);
 }
-
 
 // Find total value of all cars in stock
 // Map array of cars to an array of their prices
@@ -52,9 +43,10 @@ Dealership.prototype.totalValueOfCars = function(){
 
 module.exports = {
     Dealership,
-    count,
-    addCar,
-    getManufacturers,
-    findByManufacturer,
-    totalValueOfCars
+    // // count,
+    // addCar,
+    // getManufacturers,
+    // findByManufacturer,
+    // totalValueOfCars
 };
+  
--- dealership.test.js
+++ dealership.test.js
@@ -1,14 +1,54 @@
-const {Dealership,
-    count,
-    addCar,
-    getManufacturers,
-    findByManufacturer,
-    totalValueOfCars
+const {
+    Dealership,
+    // count,
+    // addCar,
+    // getManufacturers,
+    // findByManufacturer,
+    // totalValueOfCars   
 } = require('./dealership');
+
+const Car = require('./car');
+
+// make some cars
 
 describe('dealership tests', () =>{
 
-    test()
+    test('can initialise dealership', () => {
+        // arrange
+        const dealer = new Dealership("pushers", 5, null);
+        // act
+        const actual = dealer.name;
+        const expected = "pushers";
+        // assert
+        expect(actual).toBe(expected);
+        
+    })
+
+    test('can count total number of cars', () => {
+        // arrange
+        const car1 = new Car("tesla", 5, "3 cylinder");
+        const dealer = new Dealership("pushers", 6, [car1])
+        // act
+        const actual = dealer.count();
+        const expected = 1;
+        // assert
+        expect(actual).toBe(expected);
+    })
+
+    test('can add a car', () => {
+        // arrange
+        const car1 = new Car("tesla", 5, "3 cylinder");
+        const car2 = new Car("mercedes", 6, "4 cylinder");
+        const dealer = new Dealership("pushers", 6, [car1]);
+        dealer.addCar(car2);
+        // act
+        const actual = dealer.currentCars.length
+        const expected = 2
+        // assert
+        expect(actual).toBe(expected);
+    })
+    
+
 
 
 
